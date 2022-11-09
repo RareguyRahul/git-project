d86ade18819877a3686276d5747e94fe28cc2ce5
Rahul <rahuldharavath123@gmail.com>
2022-11-10 01:13:48 +0530

    second comit

--- Assignment2/src/MergeSort.java
+++ Assignment2/src/MergeSort.java
@@ -1,81 +1,52 @@
-class Merge {  
-void merge(int a[], int si, int mid, int ei)    
-{    
-    int i, j, k;  
-    int n1 = mid - si + 1;    
-    int n2 = ei - mid;    
-    
-        int Start[] = new int[n1];  
-        int End[] = new int[n2];  
- 
-    for (i = 0; i < n1; i++)    
-    Start[i] = a[si + i];    
-    for (j = 0; j < n2; j++)    
-    End[j] = a[mid + 1 + j];    
-      
-    i = 0;
-    j = 0; 
-    k = si; 
-      
-    while (i < n1 && j < n2)    
-    {    
-        if(Start[i] <= End[j])    
-        {    
-            a[k] = Start[i];    
-            i++;    
-        }    
-        else    
-        {    
-            a[k] =End[j];    
-            j++;    
-        }    
-        k++;    
-    }    
-    while (i<n1)    
-    {    
-        a[k] =Start[i];    
-        i++;    
-        k++;    
-    }    
-      
-    while (j<n2)    
-    {    
-        a[k] =End[j];    
-        j++;    
-        k++;    
-    }    
-}    
-  
-void mergeSort(int a[], int si, int ei)  
-{  
-    if (si < ei)   
-    {  
-        int mid = (si + ei) / 2;  
-        mergeSort(a, si, mid);  
-        mergeSort(a, mid + 1, ei);  
-        merge(a, si, mid, ei);  
-    }  
-}  
-void printArray(int a[], int n)  
-{  
-    int i;  
-    for (i = 0; i < n; i++)  
-        System.out.print(a[i] + " ");  
-}  
-  
+class Merge  {  
+	public static void conquer(int ar[],int si,int mid,int ei) {
+		int merge[]=new int[ei-si+1];
+		
+		int idx1=si;
+		int idx2=mid+1;
+		int x=0;
+		
+		while ( idx1 <=mid && idx2 <=ei){
+			if(ar[idx1] <= ar[idx2]){
+				merge[x++]=ar[idx1++];
+			}else {
+				merge[x++]=ar[idx2++];
+			}
+			
+		}
+
+		while(idx1<=mid) {
+			merge[x++]=ar[idx1++];
+		}
+		while(idx2<=ei) {
+			merge[x++]=ar[idx2++];
+		}
+		for(int i=0,j=si;i<merge.length;i++,j++) {
+				ar[j]=merge[i];
+			}
+				
+			} 
+	public static void divide(int ar[],int si, int ei) {
+		if (si>=ei) {
+		return;
+	}
+		int mid=si+(ei-si)/2;
+		divide(ar, si, mid);
+		divide(ar, mid+1, ei);
+		conquer(ar, si, mid, ei);
+	}
 public static void main(String args[])  
 {  
-    int a[] = { 11, 30, 24, 7, 31, 16, 39, 41 };  
-    int n = a.length;  
-    Merge m1 = new Merge();  
-    System.out.println("\nBefore sorting array elements are: ");  
-    m1.printArray(a, n);   
-    m1.mergeSort(a, 0, n - 1);  
-    System.out.println("\nAfter sorting array elements are: ");  
-    m1.printArray(a, n);  
-    System.out.println("");  
+   int []ar= {6,3,9,5,2,8};
+   int n=ar.length;
+   divide(ar,0,n-1);
+   for (int i=0;i<ar.length;i++) {
+	   System.out.print(ar[i]+" ");
+   }
+   System.out.println();
 	
 
 	}
 
 }
+
